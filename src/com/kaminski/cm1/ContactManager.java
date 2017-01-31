6df2fb00d42fcd36d273a74b8a28a52b0e22e7e3
VM01 <VM01@10.0.2.15>
2017-01-31 21:54:13 +0100

    final changes

--- src/com/kaminski/cm1/ContactManager.java
+++ src/com/kaminski/cm1/ContactManager.java
@@ -50,15 +50,21 @@ public class ContactManager {
 
     public void action1() {
 	System.out.println("action1()");
+	session.beginTransaction();
 	Contact contact1 = new Contact("Nam", "hainatuatgmail.com", "Vietnam", "0904277091");
 	session.persist(contact1);
+	session.flush();
+	session.getTransaction().commit();
     }
 
     public void action2() {
 	System.out.println("action2()");
+	session.beginTransaction();
 	Contact contact2 = new Contact("Bill", "billatgmail.com", "USA", "18001900");
 	Serializable id = session.save(contact2);
 	System.out.println("created id: " + id);
+	session.flush();
+	session.getTransaction().commit();
     }
 
     public void action3() {
@@ -70,72 +76,73 @@ public class ContactManager {
 
     public void action4() {
 	System.out.println("action4()");
-	// loads an object which is assumed exists
-	Contact contact4 = (Contact) session.load(Contact.class, new Integer(4));
+	// 1. session.load()
+	// It will always return a “proxy” (Hibernate term) without hitting the
+	// database. In Hibernate, proxy is an object with the given identifier
+	// value, its properties are not initialized yet, it just look like a
+	// temporary fake object.
+	// If no row found , it will throws an ObjectNotFoundException.
+	Contact contact4 = (Contact) session.load(Contact.class, new Integer(1));
 	System.out.println("Contact4's name: " + contact4.getName());
+	checkContact(contact4, 1);
     }
 
     public void action5() {
 	System.out.println("action5()");
 	// updates a loaded instance of a Contact object
+	session.beginTransaction();
 	Contact contact5 = (Contact) session.load(Contact.class, new Integer(1)); // h1
 	checkContact(contact5, 1);
 	contact5.setEmail("info1atcompany.com");
 	contact5.setTelephone("1234567890");
 	System.out.println("After Update: " + contact5);
 	session.update(contact5);
+	session.flush();
+	session.getTransaction().commit();
     }
 
     public void action6() {
 	System.out.println("action6()");
 	// updates a detached instance of a Contact object
-	Contact contact6 = new Contact(3, "Jobs", "jobsatapplet.com", "Cupertino", "0123456789");
+	session.beginTransaction();
+	Contact contact6 = new Contact(13, "Jobs", "jobsatapplet.com", "Cupertino", "0123456789");
+	Serializable id = session.save(contact6);
+	System.out.println("created id: " + id);
+	session.flush();
+	session.getTransaction().commit();
+
+	contact6.setTelephone("1111111111");
 	session.update(contact6);
     }
 
     public void action7() {
 	System.out.println("action7()");
-	// deletes an object
-	Contact contact7 = new Contact();
-	contact7.setId(7);
-	session.delete(contact7);
+	// deletes a loaded instance of an object
+	session.beginTransaction();
+	Contact contact2_Bill = (Contact) session.load(Contact.class, new Integer(2));
+	session.delete(contact2_Bill);
+	session.flush();
+	session.getTransaction().commit();
     }
 
     public static void main(String[] args) {
 
 	ContactManager cMgr = produceContactManager();
 
-	cMgr.session.beginTransaction();
 	cMgr.action1();
-	cMgr.session.getTransaction().commit();
 
-	cMgr.session.beginTransaction();
 	cMgr.action2();
-	cMgr.session.getTransaction().commit();
 
 	cMgr.action3();
 
-	cMgr.session.beginTransaction();
-	cMgr.action5();
-	cMgr.session.getTransaction().commit();
-	
-	cMgr.session.beginTransaction();
-	cMgr.action7();
-	cMgr.session.getTransaction().commit();
-
-	// Throws OptimisticLockException
-	cMgr.session.beginTransaction();
-	cMgr.action6();
-	cMgr.session.getTransaction().commit();
-
-	// Throws StandardEntityNotFoundDelegate.handleEntityNotFound
 	cMgr.action4();
 
-	cMgr.session.close();
+	cMgr.action5();
 
-	// // deletes a loaded instance of an object
-	// Contact contact8 = (Contact) session.load(Contact.class, new
-	// Integer(8));
-	// session.delete(contact8);
+	cMgr.action6();
+
+	cMgr.action7();
+
+	cMgr.session.close();
     }
 }
